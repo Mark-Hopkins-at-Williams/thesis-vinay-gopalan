1d5e83c5d864b3128b4b24b1c5291488743a36ad
gopalanvinay <gopalanvinay@gmail.com>
2019-10-30 16:09:15 -0700

    Updated clean_data to clean up usernames.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 #### DATA LINKS
 
-The clean URL version of the data can be found at: 
+The clean training data can be found at:
 
-https://drive.google.com/file/d/16qTqHdDgfOpL8BQ3HG9REg5IMI7_tZq_/view?usp=sharing
+https://drive.google.com/open?id=1HEv4T1ler395YVoJi6_CMk1_9gg5Ua2Z
 
 #### TWITTER CORPORA
 
--- clean_data.py
+++ clean_data.py
@@ -5,11 +5,11 @@ with open('train_conll.txt','r') as data:
         url_flag = 0
         url_items = []
         for line in data:
-            if url_flag == 0:
+            if url_flag == 0 and "https" not in line:
                 clean_data.write(line)
-            if "…" in line:
+            if "https" in line:
                 url_flag = 1
-            if url_flag == 1 and "…" not in line and line != "\n":
+            if url_flag == 1 and line != "\n":
                 url_items.append(line)
             if line == "\n" and url_flag == 1:
                 url = [x.replace('\t','').replace('\n','') for x in url_items]
@@ -29,3 +29,40 @@ with open('train_conll.txt','r') as data:
                 clean_data.write("\n")
                 url_flag = 0
                 url_items = []
+
+
+with open('clean_train.txt','r') as url_clean_data:
+    with open('final_train.txt','w') as final_data:
+        user_flag = 0
+        user_items = []
+        for line in url_clean_data:
+            if "@" in line or "_" in line:
+                user_items.append(line)
+                user_flag = 1
+                
+            if user_flag == 0:
+                if len(user_items) > 0:
+                    user = [x.replace('\t','').replace('\n','') for x in user_items]
+
+                    for i in range(0,len(user)):
+                        if 'Eng' in user[i]:
+                            item = user[i].replace('Eng','')
+                            user[i] = item
+                        if 'Hin' in user[i]:
+                            item = user[i].replace('Hin','')
+                            user[i] = item
+                        if 'O' in user[i]:
+                            item = user[i].replace('O','')
+                            user[i] = item
+                    user_string = ''.join(user)
+                    final_data.write(user_string + "\t" + "O")
+                    final_data.write("\n")
+                    user_items = []
+
+                if "@" not in line and "_" not in line:
+                    final_data.write(line)
+
+            
+            if user_flag == 1 and "@" not in line and "_" not in line:
+                user_items.append(line)
+                user_flag = 0
