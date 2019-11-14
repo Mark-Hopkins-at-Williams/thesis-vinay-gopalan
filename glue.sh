958b092e865d85a53640f9c69b716a903df207c7
gopalanvinay <gopalanvinay@gmail.com>
2019-11-13 16:40:49 -0800

    Updated glue.sh for multilingual case

--- glue.sh
+++ glue.sh
@@ -1,13 +1,14 @@
 export GLUE_DIR=data
 export TASK_NAME=SST-2
 
-python3 ./examples/run_glue.py \
+python3 run_glue.py \
     --model_type bert \
     --model_name_or_path bert-base-multilingual-cased \
     --task_name $TASK_NAME \
     --do_train \
     --do_eval \
     --do_lower_case \
+    --overwrite_output_dir  \
     --data_dir $GLUE_DIR/$TASK_NAME \
     --max_seq_length 128 \
     --per_gpu_eval_batch_size=8   \
