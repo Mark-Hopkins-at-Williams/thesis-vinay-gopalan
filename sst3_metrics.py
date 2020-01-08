a6196dceee1e25e23a9cf80f2036c3491490a8ee
gopalanvinay <gopalanvinay@gmail.com>
2020-01-09 01:50:42 +0530

    Formatted eval_results.txt

--- sst3_metrics.py
+++ sst3_metrics.py
@@ -4,6 +4,9 @@ def sst3_compute_metrics(task_name, preds, labels):
     """Metrics for computing SST-3 task."""
     assert len(preds) == len(labels)
     if task_name == "sst-3":
-        return {"acc": preds}
+        return {
+                "pred": preds,
+                "actual": labels
+                }
     else:
         raise KeyError(task_name)
