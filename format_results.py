e64a8cc37ea314494705855d32c6b7bfba28eafc
gopalanvinay <gopalanvinay@gmail.com>
2020-02-15 21:23:16 -0800

    Used json.load instead of direct file load

--- format_results.py
+++ format_results.py
@@ -1,34 +1,33 @@
 from clean_data import conll_to_json
+import json
 
 def format_results(json_file, preds_file, outfile):
-    tweets_data = []
     preds = []
     with open(json_file,'r') as json_reader:
-        with open(preds_file,'r') as preds_reader:
-            for tweet in json_reader:
-                tweets_data.append(tweet)
-            for label in preds_reader:
-                preds.append(label)
+        data = json.load(json_reader)
+    with open(preds_file,'r') as preds_reader:
+        for label in preds_reader:
+            preds.append(label)
     
-    assert (len(tweets_data) == len(preds))
-    num_lines = len(tweets_data)
+    assert (len(data) == len(preds))
+    num_lines = len(data)
     with open(outfile,'w') as writer:
         writer.write('Uid,Sentiment\n')
         for i in range(num_lines-1):
             if int(preds[i]) == 0:
-                writer.write('%s,negative\n'%(tweets_data[i]['uid']))
+                writer.write('%s,negative\n'%(data[i]['uid']))
             elif int(preds[i]) == 1:
-                writer.write('%s,neutral\n'%(tweets_data[i]['uid']))
+                writer.write('%s,neutral\n'%(data[i]['uid']))
             else:
-                writer.write('%s,positive\n'%(tweets_data[i]['uid']))
+                writer.write('%s,positive\n'%(data[i]['uid']))
 
         # To ensure no blank line at EOF
         if int(preds[num_lines-1]) == 0:
-            writer.write('%s,negative\n'%(tweets_data[num_lines-1]['uid']))
+            writer.write('%s,negative\n'%(data[num_lines-1]['uid']))
         elif int(preds[num_lines-1]) == 1:
-            writer.write('%s,neutral\n'%(tweets_data[num_lines-1]['uid']))
+            writer.write('%s,neutral\n'%(data[num_lines-1]['uid']))
         else:
-            writer.write('%s,positive\n'%(tweets_data[num_lines-1]['uid']))
+            writer.write('%s,positive\n'%(data[num_lines-1]['uid']))
 
 
 if __name__ == "__main__":
